7f3ae5bfc80c9c809d17a9c4af59ba784adbe8b9
vhalme <vhalme@gmail.com>
2013-01-10 18:13:01 +0200

    basic view routing

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -2,91 +2,91 @@ var App = Ember.Application.create();
 
 App.ApplicationController = Ember.Controller.extend();
 App.ApplicationView = Ember.View.extend({
-  templateName: 'application'
+	templateName: 'application'
 });
 
-App.Router = Ember.Router.extend({
-  root: Ember.Route.extend({
-    index: Ember.Route.extend({
-      route: '/'
-    })
-  })
+App.ApplicationController = Em.Controller.extend();
+App.ApplicationView = Em.View.extend({
+	templateName: 'application'
+});
+
+
+App.AddEntryController = Em.Controller.extend({
+	title: 'Add entry'
+});
+
+App.AddEntryView = Em.View.extend({
+	templateName: 'addEntryViewTemplate'
+});
+
+App.ExploreController = Em.Controller.extend({
+	title: 'Explore'	
+});
+
+App.ExploreView = Em.View.extend({
+	templateName: 'exploreViewTemplate'
+});
+
+
+App.Router = Em.Router.extend({
+	
+	enableLogging: true,
+	
+	location: 'hash',
+
+	root: Em.Route.extend({
+		
+		// EVENTS
+		viewAddEntry: Ember.Route.transitionTo('addEntry'),
+		viewExplore: Ember.Route.transitionTo('explore'),
+		
+		// STATES
+		addEntry: Em.Route.extend({
+			
+			route: '/',
+			
+			connectOutlets: function(router, context) {
+				router.get('applicationController').connectOutlet('addEntry');
+			},
+			
+			
+			// STATES
+			index: Em.Route.extend({
+				
+				route: '/',
+				connectOutlets: function(router, context) {
+					router.get('applicationController').connectOutlet('addEntry');
+				}
+				
+			})
+
+			
+		}),
+		
+		explore: Em.Route.extend({
+			
+			route: '/',
+			
+			connectOutlets: function(router, context) {
+				router.get('applicationController').connectOutlet('explore');
+			},
+
+			
+			// STATES
+			index: Em.Route.extend({
+				
+				route: '/',
+				connectOutlets: function(router, context) {
+					router.get('applicationController').connectOutlet('explore');
+				}
+				
+			})
+			
+		})
+		
+	})
+	
 });
 
 App.initialize();
 
-App.PlaceInputController = Ember.Controller.extend({
-	
-	city: undefined
-	
-});
-
-App.PageView = Ember.View.extend({
-	
-	header: App.HeaderView,
-	
-	content: App.ContentView,
-	
-	footer: App.FooterView
-	
-});
-
-
-App.HeaderView = Ember.View.extend({	
-	
-	tagName: "header"
-
-});
-
-App.PlaceInputView = Ember.TextField.extend({
-
-});
-
-App.LeftSectionView = Ember.View.extend({
-	
-	menuTitle: "I have travelled from",
-	
-	placeInput: App.PlaceInputView
-	
-	
-});
-
-App.CenterSectionView = Ember.View.extend({
-	
-});
-
-App.ContentView = Ember.View.extend({
-	
-	leftSection: App.LeftSectionView,
-	centerSection: App.CenterSectionView
-	
-});
-
-
-App.FooterView = Ember.View.extend({	
-
-	tagName: "footer"
-		
-});
-
-
-/*
-App.Person = DS.Model.extend({
-	
-	firstName: DS.attr('string'),
-	lastName: DS.attr('string'),
-
-	fullName: function() {
-		
-		return 
-			this.get('firstName') +
-	        " " + this.get('lastName');
-	  
-	}.property('firstName', 'lastName')
-	
-});
-
-App.peopleController = Em.ArrayController.create({
-	content: App.Person.findAll()
-});
-*/
